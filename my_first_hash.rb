a4d9b7b831341d911b5c59732b891f3c41c65734
Joshua Williams <jmw0426@hotmail.com>
2020-03-29 00:28:09 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,28 @@
 
+def hash_pipe
 my_hash = {"dog" => "Harley", "cat" => "Maple"}
+return my_hash
+end
+
+def shipping_manifest
+  the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+  return the_manifest
+end
+
+def retrieval
+  return shipping_manifest["oil paintings"]
+end
+
+def adding
+ shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+ 
+  shipping_manifest["muskets"] = 2
+   shipping_manifest["gun powder"] = 4
+ 
+  shipping_manifest
+end
+
+
 
 
 def shipping_manifest
